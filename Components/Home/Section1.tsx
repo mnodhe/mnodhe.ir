c7caa38aa5a10549983066a37bbee4e4cfa3e850
mnodhe <mnodhe@gmail.com>
2022-11-20 01:12:02 +0330

    added my picture

--- Components/Home/Section1.tsx
+++ Components/Home/Section1.tsx
@@ -1,9 +1,10 @@
 import { fullpageApi } from '@fullpage/react-fullpage'
 import { Button, Divider, IconButton } from '@mui/material'
+import Image from 'next/image'
 import React from 'react'
 import { AiFillTwitterCircle, AiFillLinkedin } from 'react-icons/ai'
 import { RiWhatsappFill } from 'react-icons/ri'
-
+import MyPic from "../../public/Assets/mypic.png";
 export default function Section1(props: { fullpageApi: fullpageApi }) {
   return (
     <div className="section">
@@ -34,7 +35,7 @@ export default function Section1(props: { fullpageApi: fullpageApi }) {
         <div className="col-12 col-lg-7">
           <div className="col-12">
             <span className='font-merri fw-bold fs-7 fs-lg-1'>I’M </span>
-            {}
+            { }
             <span className='font-mont font-gold fs-2'>Mohammad Mohammadi Nodehi</span>
           </div>
           <div className="col-12 font-mont font-gold fs-6">A.K.A mnodhe</div>
@@ -43,19 +44,21 @@ export default function Section1(props: { fullpageApi: fullpageApi }) {
             Developer
           </div>
           <div className="row col-12 mt-5">
-            <div className="col-12 col-md-4 col-lg-4 col-xl-2">
+            <div className="col-12 col-md-4 col-lg-4 col-xl-3">
               <a href="tel:+989120747823">
                 <Button className='col-12' type='button' variant='outlined'>Contact Me</Button>
               </a>
             </div>
-            <div className="col-12 col-md-4 col-lg-4 col-xl-2 mt-3 mt-md-0 mt-lg-0 mt-xl-0 ">
+            <div className="col-12 col-md-4 col-lg-4 col-xl-3 mt-3 mt-md-0 mt-lg-0 mt-xl-0 ">
               <a href="mailto:mnodhe@gmail.com">
                 <Button className='col-12' type='button' variant='outlined'>email Me</Button>
               </a>
             </div>
           </div>
         </div>
-        <div className="col-12 col-lg-5 d-none d-lg-block">my image will be here :)</div>
+        <div className="col-12 col-lg-5 d-none d-lg-block mypictureContainer">
+          <Image src={MyPic} alt="mohammad mohammadi nodehi's picture" className=' mypicture' />
+        </div>
 
       </div>
     </div>
