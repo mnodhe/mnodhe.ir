e66310209b4e27bea6f7f5e3a979d1ce58f4202c
mnodhe <mnodhe@gmail.com>
2022-11-22 01:30:52 +0330

    add tilt

--- Components/Home/Section4.tsx
+++ Components/Home/Section4.tsx
@@ -1,13 +1,16 @@
 import { fullpageApi } from '@fullpage/react-fullpage'
 import React from 'react'
 
-export default function Section4(props:{fullpageApi:fullpageApi}) {
+export default function Section4(props: { fullpageApi: fullpageApi }) {
   return (
-    <div className="section" >
-    <p>News</p>
-    <button onClick={() => props.fullpageApi.moveSectionDown()}>
-      Click me to move down
-    </button>
-  </div>
+    <div className="section">
+      <div className="row col-12 p-5 ">
+        <h1 className='font-mont fs-2 fw-bold mt-5'>Portfolio</h1>
+
+      </div>
+      <button onClick={() => props.fullpageApi.moveSectionDown()}>
+        Click me to move down
+      </button>
+    </div>
   )
 }
--- Components/Home/section2/SkillIndividual.tsx
+++ Components/Home/section2/SkillIndividual.tsx
@@ -1,13 +1,24 @@
 import { Card, Tooltip } from '@mui/material'
 import React from 'react'
 import Image from 'next/image'
+import Tilt from 'react-parallax-tilt';
+import { useTheme } from '@tanstack/react-query-devtools/build/lib/theme';
 
 export default function SkillIndividual(props: { Image: any, Skill: string }) {
     return (
         <div className="col-12 col-md-6 col-lg-3 col-xl-2 text-center mt-3">
             {/* maybe i will use it some day : )
             <Tooltip title="Delete" placement='top'> */}
-                <Card elevation={3} className="col-12 skillsContainer p-2">
+            <Tilt
+                className="parallax-effect-glare-scale"
+                perspective={500}
+                glareEnable={true}
+                glareMaxOpacity={0.5}
+                scale={1.02}
+                glarePosition="all"
+            >
+
+                <Card elevation={3} className="col-12 skillsContainer inner-element p-2">
                     <div className="col-12 imgcontainer m-auto">
                         <Image alt={props.Skill} src={props.Image} className="myskillIcon" />
                     </div>
@@ -15,6 +26,9 @@ export default function SkillIndividual(props: { Image: any, Skill: string }) {
                         {props.Skill}
                     </h2>
                 </Card>
+
+
+            </Tilt>
             {/* </Tooltip> */}
         </div>
     )
