c64270fa6e19a38a480123688cc9eefd11d9c14b
mnodhe <mnodhe@gmail.com>
2022-11-23 02:04:17 +0330

    add section 4 responsive

--- Components/Home/Section4.tsx
+++ Components/Home/Section4.tsx
@@ -6,18 +6,50 @@ import ghasedak from '../../public/Assets/portfolio/ghasedak.JPG'
 import gisheh7 from '../../public/Assets/portfolio/gisheh7.JPG'
 import shahpar from '../../public/Assets/portfolio/shahpar.JPG'
 import tabinu from '../../public/Assets/portfolio/tabinu.JPG'
+import aviation from '../../public/Assets/portfolio/aviation.JPG'
 
 export default function Section4(props: { fullpageApi: fullpageApi }) {
   return (
     <div className="section">
-      <div className="row col-12 p-5">
+      <div className="row col-12 p-0 mt-5">
         <h1 className='font-mont fs-1 fw-bold text-center mt-5'>Portfolio</h1>
         <div className="row mx-0">
-          <PortfolioIndividual image={cinematicket} imageAlt="cinematicket" />
-          <PortfolioIndividual image={ghasedak} imageAlt="ghasedak" />
-          <PortfolioIndividual image={gisheh7} imageAlt="gisheh7" />
-          <PortfolioIndividual image={shahpar} imageAlt="shahpar" />
-          <PortfolioIndividual image={tabinu} imageAlt="tabinu" />
+          <PortfolioIndividual
+          image={aviation}
+          imageAlt="Aviation Industry"
+          summery='weight & Balance - ULD Tracking system are being used globally in national Airlines'
+          Technology={["ReactJS","React-Query","Axios","SCSS","Ant-design"]}
+          />
+          <PortfolioIndividual
+          image={cinematicket}
+          imageAlt="cinematicket" 
+          summery='developing and maintaining the existed windows/web appication'
+          Technology={[".Net Framework","Html","CSS","bootstrap", "Responsive design","SEO"]}
+          />
+          <PortfolioIndividual
+          image={ghasedak}
+          imageAlt="ghasedak"
+          summery='An application to sell books online!by the way! Created in 4 Days :)'
+          Technology={["React","Axios","SCSS","stripe"]}
+          />
+          <PortfolioIndividual
+          image={gisheh7}
+          imageAlt="gisheh7" 
+          summery='Online cinema seat sale system.'
+          Technology={["NextJS","SCSS","Material UI","team building",".Net Framework","PostgreSQL","SEO"]}
+          />
+          <PortfolioIndividual
+          image={tabinu}
+          imageAlt="tabinu.com"
+          summery='influencer marketing automation system'
+          Technology={["NextJS","Redux","WebSocket","Axios","SEO"]}
+          />
+          <PortfolioIndividual
+          image={shahpar}
+          imageAlt="Shahpar CRM" 
+          summery='Internal ERP , VOIP & organizational chart management system'
+          Technology={["React","Redux","MUI","Responsive"]}
+          />
         </div>
       </div>
 
--- Components/Home/section4/PortfolioIndividual.tsx
+++ Components/Home/section4/PortfolioIndividual.tsx
@@ -1,23 +1,41 @@
-import { Card } from '@mui/material';
-import React from 'react'
+import { Badge, Card, Chip } from '@mui/material';
+import React, { useState } from 'react'
 import Tilt from 'react-parallax-tilt';
 import Image from 'next/image'
 
-export default function PortfolioIndividual(props: { image: any, imageAlt: string }) {
+export default function PortfolioIndividual(props: { image: any, imageAlt: string,summery:string,Technology:string[] }) {
+    const [hover, setHover] = useState(false)
     return (
-        <div className='col-12 col-lg-4 text-center mt-3'>
-            <Tilt
-                className="parallax-effect-glare-scale"
-                perspective={500}
-                glareEnable={true}
-                glareMaxOpacity={0.5}
-                scale={1.02}
-                glarePosition="all"
-            >
-                <Card elevation={3} className="col-12 skillsContainer inner-element p-2">
-                    <Image src={props.image} alt={props.imageAlt} className="col-12 portfolioimage" />
-                </Card>
-            </Tilt>
+        <div className='col-12 col-md-12 col-lg-6 col-xl-6 col-xxl-3 text-center mt-3'>
+            <Card  
+                elevation={1}
+                className="col-12 skillsContainer inner-element p-0"
+                onMouseEnter={()=>setHover(true)}
+                onMouseLeave={()=>setHover(false)}
+                style={{height: "310px"}}
+                >
+                    <div className={hover?'portfoliohovertitle font-merri':'d-none'}>
+                      <div className="col-12  font-mont font-gold">
+                        {props.imageAlt}
+                      </div>
+                      <div className="col-12 fs-5">
+                        {props.summery}
+                      </div>
+                      <div className="col-12">
+                        {props.Technology.map((tech)=>{
+                            return(
+                                <Chip clickable label={tech} size="small"/>
+                            )
+                        })}
+                      </div>
+                      
+                    </div>
+                <Image 
+                    src={props.image}
+                    alt={props.imageAlt} 
+                    className={hover?"col-12 portfolioimage blur":"col-12 portfolioimage"}
+                />
+            </Card>
         </div>
     )
 }
